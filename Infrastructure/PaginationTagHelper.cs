101f4ab673f64e4f8acf265cfb5f23d3d7516081
Bennett McCullough <mccullough.bennett@gmail.com>
2023-04-11 14:07:14 -0600

    Yo

--- Infrastructure/PaginationTagHelper.cs
+++ Infrastructure/PaginationTagHelper.cs
@@ -28,6 +28,14 @@ namespace Intex2A.Infrastructure
         public PageInfo PageBlah { get; set; }
 
         public string PageAction { get; set; }
+
+        public string PageClass { get; set; }
+
+        public bool PageClassesEnabled { get; set; }
+
+        public string PageClassNormal { get; set; }
+
+        public string PageClassSelected { get; set; }
         public override void Process (TagHelperContext thc, TagHelperOutput tho)
         {
             IUrlHelper uh = uhf.GetUrlHelper(vc);
@@ -40,6 +48,13 @@ namespace Intex2A.Infrastructure
 
                 tb.Attributes["href"] = uh.Action(PageAction, new { pageNum = i });
 
+                if (PageClassesEnabled)
+                {
+                    tb.AddCssClass(PageClass);
+                    tb.AddCssClass(i == PageBlah.CurrentPage ? PageClassSelected : PageClassNormal);
+                }
+
+                tb.AddCssClass(PageClass);
                 tb.InnerHtml.Append(i.ToString());
 
                 final.InnerHtml.AppendHtml(tb);
