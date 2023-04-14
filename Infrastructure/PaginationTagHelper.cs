d4fda70e8c1dfc1b5b3f026aea7c4436944ede27
Bennett McCullough <mccullough.bennett@gmail.com>
2023-04-13 23:13:41 -0600

    Pagination and what not

--- Infrastructure/PaginationTagHelper.cs
+++ Infrastructure/PaginationTagHelper.cs
@@ -35,27 +35,52 @@ namespace Intex2A.Infrastructure
 
         public string PageClassNormal { get; set; }
 
+        public string PageClassNuts { get; set; }
+
         public string PageClassSelected { get; set; }
         public override void Process (TagHelperContext thc, TagHelperOutput tho)
         {
             IUrlHelper uh = uhf.GetUrlHelper(vc);
 
-            TagBuilder final = new TagBuilder("span");
+            TagBuilder final = new TagBuilder("div");
 
-            for (int i = 1; i < PageBlah.TotalPages; i++)
+            int startPage = PageBlah.CurrentPage - 3;
+            int endPage = PageBlah.CurrentPage + 4;
+            // Make sure we don't have negative page numbers anymore lmfaooo
+            if (PageBlah.CurrentPage == 1 || PageBlah.CurrentPage == 2 || PageBlah.CurrentPage == 3)
+            {
+                startPage = 1;
+            }
+            if (PageBlah.CurrentPage == PageBlah.TotalPages || PageBlah.CurrentPage == PageBlah.TotalPages-1 || PageBlah.CurrentPage == PageBlah.TotalPages-2 || PageBlah.CurrentPage == PageBlah.TotalPages - 3)
+            {
+                endPage = PageBlah.TotalPages;
+            }
+            
+
+            for (int i = startPage; i < endPage; i++)
             {
                 TagBuilder tb = new TagBuilder("a");
+                string pageName = "";
 
+                if (i == PageBlah.CurrentPage)
+                {
+                    pageName = "(" + i.ToString() + ")";
+                }
+                else
+                {
+                    pageName = i.ToString();
+                }
                 tb.Attributes["href"] = uh.Action(PageAction, new { pageNum = i });
 
                 if (PageClassesEnabled)
                 {
+                    tb.AddCssClass(PageClassNuts);
                     tb.AddCssClass(PageClass);
                     tb.AddCssClass(i == PageBlah.CurrentPage ? PageClassSelected : PageClassNormal);
                 }
 
                 tb.AddCssClass(PageClass);
-                tb.InnerHtml.Append(i.ToString());
+                tb.InnerHtml.Append(pageName.ToString());
 
                 final.InnerHtml.AppendHtml(tb);
             }
@@ -64,3 +89,24 @@ namespace Intex2A.Infrastructure
         }
     }
 }
+
+
+
+
+//for (int i = 1; i < PageBlah.TotalPages; i++)
+//{
+//    TagBuilder tb = new TagBuilder("a");
+
+//    tb.Attributes["href"] = uh.Action(PageAction, new { pageNum = i });
+
+//    if (PageClassesEnabled)
+//    {
+//        tb.AddCssClass(PageClass);
+//        tb.AddCssClass(i == PageBlah.CurrentPage ? PageClassSelected : PageClassNormal);
+//    }
+
+//    tb.AddCssClass(PageClass);
+//    tb.InnerHtml.Append(i.ToString());
+
+//    final.InnerHtml.AppendHtml(tb);
+//}
